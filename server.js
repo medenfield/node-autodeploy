9abd1adb0123917988b0c28a365986c80501f19e
Eddie <medenfie@scad.edu>
2018-05-03 13:58:09 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+// Our first route.
+app.get('/', function (req, res) {
+   res.send('Hello Dev!');
+});
+
+// Our second route.
+app.get('/dev', function(req, res) {
+  res.send("Hello, you are now on the dev route!");
+});
+
+// Listen to port 5000.
+app.listen(5000, function() {
+  console.log('Dev app listining on port 5000');
+});
