726738e679e99fda8356623675a4b1b488ec1899
Eddie <medenfie@scad.edu>
2018-05-03 14:27:26 -0400

    copypasta from other git repo

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 5000);
 
-// Our first route.
+// Our first route
 app.get('/', function (req, res) {
-   res.send('Hello Node + Github! This code push has auto-deployed?');
+  res.send('Hello Node + GitHub!');
 });
 
-// Our second route.
-app.get('/dev', function(req, res) {
-  res.send("Hello, you are now on the dev route!");
+// Listen to port
+app.listen(app.get('port'), function () {
+  console.log('App is listening on port ' + app.get('port'));
 });
 
 // Listen to port 5000.
